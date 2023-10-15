eae88ce3ce6e40b54a96f7d1387d95f3d074e55a
HuseyinKansiz <147552534+HuseyinKansiz@users.noreply.github.com>
2023-10-15 18:45:44 +0300

    Update odev4.java

--- example/odev4.java
+++ example/odev4.java
@@ -9,25 +9,19 @@ public class odev4 {
         int sayi0=0;
         int sayi1=0;
         int sayi = Integer.parseInt(JOptionPane.showInputDialog("Toplama icin-1\n cikarma icin-2\n bolme icin-3\n carpma icin-4\n Numara giriniz"));
+        sayi1 = Integer.parseInt(JOptionPane.showInputDialog("ilk sayiyi giriniz \n "));
+        sayi0 = Integer.parseInt(JOptionPane.showInputDialog("ikinci sayiyi giriniz \n Sonuc icin Enterlayin"));
         switch(sayi){
             case 1:
-                sayi1 = Integer.parseInt(JOptionPane.showInputDialog("Toplanacak sayi 1 i girin \n "));
-                sayi0 = Integer.parseInt(JOptionPane.showInputDialog("Toplanacak sayi 2 yi girip \n Sonuc icin Enterlayin"));
                 JOptionPane.showMessageDialog(null,sayi1+sayi0, "Cevap",JOptionPane.INFORMATION_MESSAGE);
                 break;
             case 2:
-                sayi1 = Integer.parseInt(JOptionPane.showInputDialog("Eksilen sayiyi girin \n "));
-                sayi0 = Integer.parseInt(JOptionPane.showInputDialog("Cıkan sayıyı girip \n Sonuc icin Enterlayin"));
                 JOptionPane.showMessageDialog(null,sayi1-sayi0, "Cevap",JOptionPane.INFORMATION_MESSAGE);
                 break;
             case 3:
-                sayi1 = Integer.parseInt(JOptionPane.showInputDialog("Bolunecek sayiyi girin \n "));
-                sayi0 = Integer.parseInt(JOptionPane.showInputDialog("Bolen sayiyi girip \n Sonuc icin Enterlayin"));
                 JOptionPane.showMessageDialog(null,(float)sayi1/sayi0, "Cevap",JOptionPane.INFORMATION_MESSAGE); // integer virgülden sonrasini vermeyeceginden float casting yapılır
                 break;
             case 4:
-                sayi1 = Integer.parseInt(JOptionPane.showInputDialog("Carpan sayi1 girin \n "));
-                sayi0 = Integer.parseInt(JOptionPane.showInputDialog("Carpan sayi2 girip \n Sonuc icin Enterlayin"));
                 JOptionPane.showMessageDialog(null,sayi1*sayi0, "Cevap",JOptionPane.INFORMATION_MESSAGE);
                 break;
         }
